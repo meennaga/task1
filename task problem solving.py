8eafbb5a0600d23f568654701351063a6cfd10a4
MennaElbana <mennaelbana15@gmail.com>
2024-12-31 23:03:18 +0200

    Add files via upload

---
+++ task problem solving.py
@@ -0,0 +1,9 @@
+def apply_operations(operations):
+    
+    result = 0
+    for operations in operations
+        if operations == "++"
+            result += 1
+        elif operation == "--"
+            result -= 1
+    return result
